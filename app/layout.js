2607f18488b141a3e5244f70744446f2b6748485
Atharva Mahadeokar <athaarva999@gmail.com>
2023-08-06 20:26:35 +0530

    final commit

--- app/layout.js
+++ app/layout.js
@@ -5,11 +5,11 @@ import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
 import NextTopLoader from "nextjs-toploader";
 
-export const metadata = {
-  title: "Divyang",
-  description:
-    "This is the website which facilitates the governance for the disabled person.",
-};
+// export const metadata = {
+//   title: "Divyang",
+//   description:
+//     "This is the website which facilitates the governance for the disabled person.",
+// };
 
 export default function RootLayout({ children }) {
   const [isClient, setIsClient] = useState(false);
